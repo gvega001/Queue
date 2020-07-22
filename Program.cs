02cd5e9985d14070f012336a744da9948e338d3c
Gabriel Vega <gvega001@gmail.com>
2020-07-21 20:55:14 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using static System.Console;
+
+namespace QueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQueue = new Queue(5);
+            myQueue.insert(100);
+            myQueue.insert(10);
+            myQueue.insert(20);
+            myQueue.insert(30);
+            myQueue.view();
+
+            Thread.Sleep(2000);
+        }
+    }
+}
